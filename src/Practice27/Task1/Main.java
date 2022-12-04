625c1cf207381bbe0b95a7524ee007f9e3648331
Loftiness-Spirit <krylovalex2003@mail.ru>
2022-12-05 00:09:49 +0300

    Waiting for editing

---
+++ src/Practice27/Task1/Main.java
@@ -0,0 +1,19 @@
+package Practice27.Task1;
+
+import java.util.HashSet;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        HashSet<String> hashSet = new HashSet<String>();
+        hashSet.add("Леонид");
+        hashSet.add("Иван");
+        hashSet.add("Андрей");
+        hashSet.add("Денис");
+        System.out.println(hashSet);
+
+        TreeSet<String> treeSet = new TreeSet<String>(hashSet);
+        System.out.println(treeSet);
+    }
+}
