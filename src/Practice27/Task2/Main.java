6c6bf4163f9e5fd891ad060f91838d206107db4a
Loftiness-Spirit <krylovalex2003@mail.ru>
2022-12-22 21:50:33 +0300

    FIX

--- src/Practice27/Task2/Main.java
+++ src/Practice27/Task2/Main.java
@@ -8,13 +8,13 @@ public class Main {
         map.Add("Иванов","Иван");
         map.Add("Петров","Иван");
         map.Add("Олбутов","Дима");
-        map.Add("Петров","Петр");
+        map.Add("Дубов","Петр");
 
         map.Add("Крылов","Алесандр");
-        map.Add("Иванов","Иван");
-        map.Add("Петров","Иван");
-        map.Add("Олбутов","Дима");
-        map.Add("Петров","Петр");
+        map.Add("Кашин","Олег");
+        map.Add("Мандаринов","Никита");
+        map.Add("Стеклов","Женя");
+        map.Add("Яблочников","Костя");
         System.out.println(map.getSameFirstNameCount());
         System.out.println(map.getSameLastNameCount());
         map.CreateMap();
